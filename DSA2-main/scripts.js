d24a27be022a995b61a9fc5de11180ec20dc6c43
Matosaz <Mathusphd20@gmail.com>
2024-03-18 08:26:31 -0300

    Formulário

--- DSA2-main/scripts.js
+++ DSA2-main/scripts.js
@@ -2,7 +2,27 @@ var botao = document.getElementById("botao");
 botao.addEventListener("click", alertar);
 
 var nome = document.getElementById("nome");
+var saida = document.getElementById("saida-de-dados");
+var email = document.getElementById("email");
+var tel = document.getElementById("tel");
+var CEP = document.getElementById("CEP");
+var logr = document.getElementById("logr");
+var num = document.getElementById("num");
+var comp = document.getElementById("comp");
+var bair = document.getElementById("bair");
+var cit = document.getElementById("cit");
+var est = document.getElementById("est");
 
 function alertar(){
-    alert("O nome inserido foi" + nome);
+   // alert(nome.value +"clicou no botão: " + event.target.value);
+    saida.innerText = "Nome: " + nome.value + 
+    "\n E-mail: " + email.value +
+    "\n Telefone: " + tel.value +
+    "\n CEP: " + CEP.value +
+    "\n Logradouro: " + logr.value +
+    "\n Número: " + num.value +
+    "\n Complemento: " + comp.value +
+    "\n Bairro: " + bair.value +
+    "\n Cidade: " + cit.value +
+    "\n Estado: " +est.value;
 }
