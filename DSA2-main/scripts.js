7fed1791eced27891e16529aaadec19fe7357da7
Matosaz <Mathusphd20@gmail.com>
2024-03-18 07:11:35 -0300

    Projeto Inicial JS

---
+++ DSA2-main/scripts.js
@@ -0,0 +1,8 @@
+var botao = document.getElementById("botao");
+botao.addEventListener("click", alertar);
+
+var nome = document.getElementById("nome");
+
+function alertar(){
+    alert("O nome inserido foi" + nome);
+}
